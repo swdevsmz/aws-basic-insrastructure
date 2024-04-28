dfe057df0ea5556f1806279e56b0f813a8df5601
swdev.smz <duke2per@gmail.com>
2024-04-28 09:59:55 +0900

    subnet追加

--- aws_vpc.tf
+++ aws_vpc.tf
@@ -1,10 +1,47 @@
+variable "name" {
+  type    = string
+  default = "main"
+}
+
+variable "azs" {
+  type    = list(string)
+  default = ["ap-northeast-1a", "ap-northeast-1c", "ap-northeast-1d"]
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  default = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  default = ["10.0.10.0/24", "10.0.11.0/24", "10.0.12.0/24"]
+}
+
+
 #----------------------------------------
 # VPCの作成
 #----------------------------------------
 resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
   tags = {
-    Name = "main"
+    Name = var.name
+  }
+}
+
+# Public Subnet
+resource "aws_subnet" "publics" {
+  count = length(var.public_subnet_cidrs)
+
+  vpc_id = aws_vpc.main.id
+
+  availability_zone = var.azs[count.index]
+  cidr_block        = var.public_subnet_cidrs[count.index]
+
+  tags = {
+    Name = "${var.name}-public-${count.index}"
   }
 }
